13579bcfe3f5c9c9b9be7df3e4ee94da4ea589be
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2020-11-09 19:15:42 -0800

    added DayPilot calendar

---
+++ bookIt/src/main/java/com/codingdojo/bookIt/controllers/CalendarController.java
@@ -0,0 +1,123 @@
+package com.codingdojo.bookIt.controllers;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingdojo.bookIt.models.Event;
+import com.codingdojo.bookIt.repositories.EventRepository;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@RestController
+public class CalendarController {
+
+	//TO-DO: Needs a Service -- fix later
+    @Autowired
+    EventRepository er;
+
+    @RequestMapping("/api")
+    @ResponseBody
+    String home() {
+        return "Welcome!";
+    }
+
+    @GetMapping("/api/events")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    Iterable<Event> events(@RequestParam("start") @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime start, @RequestParam("end") @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime end) {
+        return er.findBetween(start, end);
+    }
+
+    @PostMapping("/api/events/create")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Transactional
+    Event createEvent(@RequestBody EventCreateParams params) {
+
+        Event e = new Event();
+        e.setStart(params.start);
+        e.setEnd(params.end);
+        e.setText(params.text);
+        er.save(e);
+
+        return e;
+    }
+
+    @PostMapping("/api/events/move")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Transactional
+    Event moveEvent(@RequestBody EventMoveParams params) {
+
+    	//This seems to be a Service Method  --- needs to be rewritten.
+        Event e = er.findById(params.id).orElse(null);
+        e.setStart(params.start);
+        e.setEnd(params.end);
+        er.save(e);
+
+        return e;
+    }
+
+    @PostMapping("/api/events/setColor")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Transactional
+    Event setColor(@RequestBody SetColorParams params) {
+
+    	//This seems to be a Service Method  --- needs to be rewritten.
+    	Event e = er.findById(params.id).orElse(null);
+        e.setColor(params.color);
+        er.save(e);
+
+        return e;
+    }
+
+    @PostMapping("/api/events/delete")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Transactional
+    EventDeleteResponse deleteEvent(@RequestBody EventDeleteParams params) {
+
+    	//This seems to be a Service Method  --- needs to be rewritten.
+        er.deleteById(params.id);
+
+        return new EventDeleteResponse() {{
+            message = "Deleted";
+        }};
+    }
+
+    //What are these Methods? Are these supposed to be in the Service? Explore Later
+    public static class EventDeleteParams {
+        public Long id;
+    }
+
+    public static class EventDeleteResponse {
+        public String message;
+    }
+
+    public static class EventCreateParams {
+        public LocalDateTime start;
+        public LocalDateTime end;
+        public String text;
+    }
+
+    public static class EventMoveParams {
+        public Long id;
+        public LocalDateTime start;
+        public LocalDateTime end;
+    }
+
+    public static class SetColorParams {
+        public Long id;
+        public String color;
+    }
+
+
+}
